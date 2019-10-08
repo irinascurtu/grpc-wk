08683e4991e0b4fe9da9a2eb517bd2ee44775fb3
Irina Scurtu <scurtuirina@gmail.com>
2019-10-08 16:52:37 +0300

    added grpc

---
+++ Grpc/Unary.Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Grpc.Core;
+
+namespace Unary.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Channel channel = new Channel("127.0.0.1:5001", ChannelCredentials.Insecure);
+           
+            // using var channel = GrpcChannel.ForAddress("http://localhost:5001");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
